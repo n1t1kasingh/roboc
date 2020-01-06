21be086d4e8f23fc2dcbcf0a34a08d59faa69fbc
n1t1kasingh <nikativikata.singh@gmail.com>
2020-01-05 22:11:12 -1100

    Splash screen

--- app/src/main/java/com/nitikasingh/roboc/MainActivity.java
+++ app/src/main/java/com/nitikasingh/roboc/MainActivity.java
@@ -3,15 +3,23 @@ package com.nitikasingh.roboc;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout dl;
     private ActionBarDrawerToggle abdt;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +28,28 @@ public class MainActivity extends AppCompatActivity {
         abdt=new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
         abdt.setDrawerIndicatorEnabled(true);
 
-
         dl.addDrawerListener(abdt);
         abdt.syncState();
 
+
+        LinearLayout gallery=findViewById(R.id.gallery);
+        LayoutInflater inflater=LayoutInflater.from(this);
+
+
+
         NavigationView nav_view=(NavigationView)findViewById(R.id.nav_view);
-        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+        for(int i=0;i<6;i++) {
+            View view = inflater.inflate(R.layout.item, gallery, false);
+            TextView textView = view.findViewById(R.id.withText);
+            textView.setText("Item" + i);
+            ImageView imageView = findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.ic_launcher_background);
+
+            gallery.addView(view);
+        }
+
+            nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id= menuItem.getItemId();
@@ -56,7 +80,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-    }
+
+        }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
